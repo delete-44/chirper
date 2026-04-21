82c290174f45f519b6dea16d900aa960742dbe7a
delete-44 <delete44+tech@protonmail.com>
2026-04-21 11:33:50 +0100

    Chrip template added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,7 @@
   <div class="max-w-2xl mx-auto">
     <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
     @forelse ($chirps as $chirp)
-      <div class="card bg-base-100 shadow mt-4">
-        <div class="card-body">
-          <div>
-            <div class="font-semibold">{{ $chirp->user ? $chirp->user->name : 'Anonymous' }}</div>
-            <div class="mt-1">{{ $chirp->message }}</div>
-            <div class="text-sm text-gray-500 mt-2">{{ $chirp->created_at->diffForHumans() }}</div>
-          </div>
-        </div>
-      </div>
+      <x-chirp :chirp="$chirp" />
     @empty
     <p class="text-gray-400">No chirps yet. Be the first to chirp!</p>
     @endif
