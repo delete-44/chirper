e1a4a51b1ba36ea07caabdd2d3100889392ba6af
delete-44 <delete44+tech@protonmail.com>
2026-04-21 10:42:59 +0100

    Showing mock chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -6,7 +6,77 @@ use Illuminate\Http\Request;
 
 class ChirpController extends Controller
 {
-    public function index() {
-        return view("home");
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $chirps = [
+            [
+                'author' => 'Rincewind',
+                'message' => 'Can anybody hear me',
+                'time' => '5 minutes ago'
+            ],
+            [
+                'author' => 'Slamdunk Johnson',
+                'message' => 'I\'m learning Excel! Do you know Excel?',
+                'time' => '1 hour ago'
+            ],
+            [
+                'author' => 'Grognor the Relentless',
+                'message' => 'Hmmm',
+                'time' => '3 hours ago'
+            ]
+        ];
+
+        return view("home", ['chirps' => $chirps]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,17 @@
   </x-slot:title>
 
   <div class="max-w-2xl mx-auto">
-    <div class="card bg-base-100 shadow mt-8">
-      <div class="card-body">
-        <div>
-          <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
-          <p class="mt-4 text-gray-900">This is your brand new Laravel application. Time to make it
-            sing (or chirp)!</p>
-          <p class="mt-2 text-gray-700"><small>This is live on the internet!</small></p>
+    <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
+    @foreach ($chirps as $chirp)
+      <div class="card bg-base-100 shadow mt-4">
+        <div class="card-body">
+          <div>
+            <div class="font-semibold">{{ $chirp['author'] }}</div>
+            <div class="mt-1">{{ $chirp['message'] }}</div>
+            <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
+          </div>
         </div>
       </div>
-    </div>
+    @endforeach
   </div>
 </x-layout>
