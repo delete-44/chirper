3964ffe80f70b40ed7183918a6adcbdf8afaeaf6
delete-44 <delete44+tech@protonmail.com>
2026-04-21 10:23:44 +0100

    Testing CD pipeline

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,9 @@
       <div class="card-body">
         <div>
           <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
-          <p class="mt-4 text-base-content/60">This is your brand new Laravel application. Time to make it
+          <p class="mt-4 text-gray-900">This is your brand new Laravel application. Time to make it
             sing (or chirp)!</p>
+          <p class="mt-2 text-gray-700"><small>This is live on the internet!</small></p>
         </div>
       </div>
     </div>
