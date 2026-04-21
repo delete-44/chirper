c2a345acb1e387a9a4b48c80dea4ca07c26ce965
delete-44 <delete44+tech@protonmail.com>
2026-04-21 16:30:42 +0100

    Populate auth controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
 use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
@@ -10,9 +12,22 @@ Route::resource('chirps', ChirpController::class)
   ->middleware(('auth'))
   ->only(['store', 'edit', 'update', 'destroy']);
 
+// Authentication routes
+
 Route::view('/register', 'auth.register')
   ->middleware('guest')
   ->name('register');
 
 Route::post('/register', Register::class)
   ->middleware('guest');
+
+Route::view('/login', 'auth.login')
+  ->middleware('guest')
+  ->name('login');
+
+Route::post('/login', Login::class)
+  ->middleware('guest');
+
+Route::post('/logout', Logout::class)
+  ->middleware('auth')
+  ->name('logout');
