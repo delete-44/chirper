0d9d16ee1e1526d3e23c03f5f2912eaeac427117
delete-44 <delete44+tech@protonmail.com>
2026-04-21 12:09:42 +0100

    Popilate chirp form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,32 @@
 
   <div class="max-w-2xl mx-auto">
     <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
+    <form method="POST" action="/chirps">
+      @csrf
+      <div class="form-control mt-4 w-full">
+        <label class="text-gray-500 text-sm" for="message">Chirp at us</label>
+        <textarea name="message" placeholder="Whats on your mind?" class="textarea textarea-bordered w-full resize-none"
+          rows="4" maxlength="255" required>
+          {{ old('message') }}
+        </textarea>
+
+        @error('message')
+          <div class="label">
+            <span class="label-text-alt text-error">{{ $message }}</span>
+          </div>
+        @enderror
+      </div>
+
+      <div class="mt-2 flex items-center justify-end">
+        <button type="submit" class="btn btn-primary btn-sm">
+          Chirp
+        </button>
+      </div>
+    </form>
     @forelse ($chirps as $chirp)
       <x-chirp :chirp="$chirp" />
     @empty
-    <p class="text-gray-400">No chirps yet. Be the first to chirp!</p>
-    @endif
+      <p class="text-gray-400">No chirps yet. Be the first to chirp!</p>
+    @endforelse
   </div>
 </x-layout>
