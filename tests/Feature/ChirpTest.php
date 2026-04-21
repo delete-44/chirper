157992757b7d987667386ef2df2134bdf9e3d5ca
delete-44 <delete44+tech@protonmail.com>
2026-04-21 19:06:14 +0100

    Popiulated remaining tests in suite

--- tests/Feature/ChirpTest.php
+++ tests/Feature/ChirpTest.php
@@ -15,18 +15,120 @@ test('index', function () {
     }
 });
 
-test('store', function () {
-    $user = User::factory()->create();
+describe('#store', function () {
+    test('store fails when user is signed out', function () {
+        $response = $this->post('/chirps', [
+            'message' => 'Test message'
+        ]);
 
-    $response = $this->actingAs($user)->post('/chirps', [
-        'message' => 'Test message'
-    ]);
+        $response->assertRedirect('/login');
 
-    $response->assertRedirect('/');
+        $this->assertDatabaseEmpty('chirps');
+    });
 
-    $this->assertDatabaseHas('chirps', [
-        'message' => 'Test message'
-    ]);
+    test('store completes when user is signed in', function () {
+        $user = User::factory()->create();
 
-    $this->assertDatabaseCount('chirps', 1);
+        $response = $this->actingAs($user)->post('/chirps', [
+            'message' => 'Test message'
+        ]);
+
+        $response->assertRedirect('/');
+
+        $this->assertDatabaseHas('chirps', [
+            'message' => 'Test message'
+        ]);
+
+        $this->assertDatabaseCount('chirps', 1);
+    });
+});
+
+describe('#update', function () {
+    test('update fails when signed out', function () {
+        $chirp = Chirp::factory()->create([
+            'message' => 'Existing message'
+        ]);
+
+        $response = $this->put("/chirps/$chirp->id", [
+            'message' => 'Updated message'
+        ]);
+
+        $response->assertRedirect('/login');
+
+        $this->assertDatabaseHas('chirps', [
+            'message' => 'Existing message'
+        ]);
+
+        $this->assertDatabaseCount('chirps', 1);
+    });
+
+    test('update fails when performed by different user to chirp owner', function () {
+        $otherUser = User::factory()->create();
+        $chirp = Chirp::factory()->create([
+            'message' => 'Existing message'
+        ]);
+
+        $response = $this->actingAs($otherUser)->put("/chirps/$chirp->id", [
+            'message' => 'Updated message'
+        ]);
+
+        $response->assertForbidden();
+
+        $this->assertDatabaseHas('chirps', [
+            'message' => 'Existing message'
+        ]);
+
+        $this->assertDatabaseCount('chirps', 1);
+    });
+
+    test('update completes when performed by chirp owner', function () {
+        $chirp = Chirp::factory()->create([
+            'message' => 'Existing message'
+        ]);
+
+        $response = $this->actingAs($chirp->user)->put("/chirps/$chirp->id", [
+            'message' => 'Updated message'
+        ]);
+
+        $response->assertRedirect('/');
+
+        $this->assertDatabaseHas('chirps', [
+            'message' => 'Updated message'
+        ]);
+
+        $this->assertDatabaseCount('chirps', 1);
+    });
+});
+
+describe('#destroy', function () {
+    test('destroy fails when signed out', function () {
+        $chirp = Chirp::factory()->create();
+
+        $response = $this->delete("/chirps/$chirp->id");
+
+        $response->assertRedirect('/login');
+
+        $this->assertDatabaseCount('chirps', 1);
+    });
+
+    test('destroy fails when performed by different user to chirp owner', function () {
+        $otherUser = User::factory()->create();
+        $chirp = Chirp::factory()->create();
+
+        $response = $this->actingAs($otherUser)->delete("/chirps/$chirp->id");
+
+        $response->assertForbidden();
+
+        $this->assertDatabaseCount('chirps', 1);
+    });
+
+    test('destroy completes when performed by chirp owner', function () {
+        $chirp = Chirp::factory()->create();
+
+        $response = $this->actingAs($chirp->user)->delete("/chirps/$chirp->id");
+
+        $response->assertRedirect('/');
+
+        $this->assertDatabaseCount('chirps', 0);
+    });
 });
